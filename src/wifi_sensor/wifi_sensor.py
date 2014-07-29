cb735dafc0f939ce524e84d1a8b9175e15f9c9ee
Christian Blum <christianb314@gmail.com>
2014-07-29 16:30:06 +0200

    * sniffing is now in its own thread * conforms to new msg standard * some minor improvments

--- src/wifi_sensor/wifi_sensor.py
+++ src/wifi_sensor/wifi_sensor.py
@@ -5,29 +5,42 @@ import time
 import scapy.all as sca
 import numpy as np
 from msg import Rssi
+import thread
 
 class WifiSensor():
   def __init__(self):
-    self.pub = rospy.Publisher("rssi_mean", Rssi, queue_size=10)
+    # setup shared data for threads and start
+    self.data = {}
+    self.dataMutex = thread.allocate_lock()
+    thread.start_new_thread(self.mesRaw, ())
+    # setup main loop
+    rospy.init_node("wifisensor")
+    self.pub = rospy.Publisher("rssi", Rssi, queue_size=10)
+    r = rospy.Rate(10) # 10Hz
     while not rospy.is_shutdown():
-      rssi = self.mesRaw()
-      msg = Rssi()
-      msg.rssi = rssi
-      msg.variance = 0.0
-      self.pub.publish(msg)
-      time.sleep(0.1)
-  def parsePacket(self, pkt):
-    if pkt.haslayer(sca.Dot11) :
-      if pkt.addr2 != None:
-        return pkt.addr2, -(256-ord(pkt.notdecoded[-4:-3]))
-    return None, None
-  def mesRaw(self, n=10):
-    data = []
-    while(len(data) < n):
-      packets = sca.sniff(iface="wlan0", count = n/5)
+      data = {}
+      with self.dataMutex:
+        data = dict(self.data)
+        self.data = {}
+      for addr in data.keys():
+        msg = Rssi()
+        msg.macaddr = addr
+        msg.rssi = data[addr]
+        self.pub.publish(msg)
+      r.sleep()
+  def mesRaw(self):
+    while not rospy.is_shutdown():
+      packets = sca.sniff(iface="wlan0", count = 10)
       for pkt in packets:
         addr, rssi = self.parsePacket(pkt)
-        # eduroam
-        if addr == "20:b3:99:1b:f7:58":
-          data.append(rssi)
-    return data
+        if addr is not None:
+          with self.dataMutex:
+            if addr in self.data.keys():
+              self.data[addr].append(rssi)
+            else:
+              self.data[addr] = [rssi]
+  def parsePacket(self, pkt):
+    if pkt.haslayer(sca.Dot11):
+      if pkt.addr2 is not None:
+        return pkt.addr2, ord(pkt.notdecoded[-4:-3])-256
+    return None, None
