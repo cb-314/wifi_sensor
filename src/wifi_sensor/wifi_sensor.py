185bb06c114963c8da5fe24c5089ffb51c48008b
Christian Blum <christianb314@gmail.com>
2014-08-01 13:02:32 +0200

    added parameters for adapter and channel

--- src/wifi_sensor/wifi_sensor.py
+++ src/wifi_sensor/wifi_sensor.py
@@ -9,14 +9,18 @@ import thread
 
 class WifiSensor():
   def __init__(self):
+    # setup main loop and rospy
+    rospy.init_node("wifisensor")
+    self.pub = rospy.Publisher("rssi", RssiMulti, queue_size=10)
+    r = rospy.Rate(5)
+    # get parameters
+    self.adapter = rospy.get_param("~adapter", "wlan0")
+    self.channel = rospy.get_param("~channel", 9)
     # setup shared data for threads and start
     self.data = {}
     self.dataMutex = thread.allocate_lock()
     thread.start_new_thread(self.mesRaw, ())
-    # setup main loop
-    rospy.init_node("wifisensor")
-    self.pub = rospy.Publisher("rssi", RssiMulti, queue_size=10)
-    r = rospy.Rate(5)
+    # main loop
     while not rospy.is_shutdown():
       data = {}
       with self.dataMutex:
@@ -34,7 +38,7 @@ class WifiSensor():
       r.sleep()
   def mesRaw(self):
     while not rospy.is_shutdown():
-      packets = sca.sniff(iface="wlan0", count = 10)
+      packets = sca.sniff(iface=self.adapter, count = 10)
       for pkt in packets:
         addr, rssi = self.parsePacket(pkt)
         if addr is not None:
