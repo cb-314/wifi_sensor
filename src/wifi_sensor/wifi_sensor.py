5066bccee7dec14b1e2ea9fa42ec0404b3250814
Christian Blum <christianb314@gmail.com>
2014-08-07 14:26:43 +0200

    updated packet parsing to a much improved method actually parsing the radiotap fields

--- src/wifi_sensor/wifi_sensor.py
+++ src/wifi_sensor/wifi_sensor.py
@@ -7,6 +7,7 @@ import numpy as np
 from msg import *
 import thread
 import subprocess
+import struct
 
 class WifiSensor():
   def __init__(self):
@@ -23,6 +24,15 @@ class WifiSensor():
     # find your own mac addr
     output = subprocess.check_output(["ifconfig", "-a", self.adapter])
     self.my_addr = "-".join([x.lower() for x in output.split(" ")[output.split(" ").index("HWaddr")+1].split("-")[:6]])
+    # Radiotap field specifications
+    self.radiotap_formats = {"TSFT":"Q", "Flags":"B", "Rate":"B",
+      "Channel":"HH", "FHSS":"BB", "dBm_AntSignal":"b", "dBm_AntNoise":"b",
+      "Lock_Quality":"H", "TX_Attenuation":"H", "dB_TX_Attenuation":"H",
+      "dBm_TX_Power":"b", "Antenna":"B",  "dB_AntSignal":"B",
+      "dB_AntNoise":"B", "b14":"H", "b15":"B", "b16":"B", "b17":"B", "b18":"B",
+      "b19":"BBB", "b20":"LHBB", "b21":"HBBBBBH", "b22":"B", "b23":"B",
+      "b24":"B", "b25":"B", "b26":"B", "b27":"B", "b28":"B", "b29":"B",
+      "b30":"B", "Ext":"B"}
     # setup shared data for threads and start
     self.data = {}
     self.dataMutex = thread.allocate_lock()
@@ -61,5 +71,23 @@ class WifiSensor():
   def parsePacket(self, pkt):
     if pkt.haslayer(sca.Dot11):
       if pkt.addr2 is not None:
-        return pkt.addr2, ord(pkt.notdecoded[-4:-3])-256
+        # check available Radiotap fields
+        field, val = pkt.getfield_and_val("present")
+        names = [field.names[i][0] for i in range(len(field.names)) if (1 << i) & val != 0]
+        # check if we measured signal strength
+        if "dBm_AntSignal" in names:
+          # decode radiotap header
+          fmt = "<"
+          rssipos = 0
+          for name in names:
+            # some fields consist of more than one value
+            if name == "dBm_AntSignal":
+              # correct for little endian format sign
+              rssipos = len(fmt)-1
+            fmt = fmt + self.radiotap_formats[name]
+          # unfortunately not all platforms work equally well and on my arm
+          # platform notdecoded was padded with a ton of zeros without
+          # indicating more fields in pkt.len and/or padding in pkt.pad
+          decoded = struct.unpack(fmt, pkt.notdecoded[:struct.calcsize(fmt)])
+          return pkt.addr2, decoded[rssipos]
     return None, None
